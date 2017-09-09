c43179371685f5245a98db48e49b0bfa1936be60
Daniel Way <email": null>
2017-09-09 13:31:28 -0700

    add votes to comments

--- public/js/application.js
+++ public/js/application.js
@@ -1,34 +1,34 @@
-This function will display the question details without refreshing page
+// This function will display the question details without refreshing page
 
-$(document).ready(function() {bindClickEvents() })
+// $(document).ready(function() {bindClickEvents() })
 
-// Intercept the form submission event using JQuery
-function bindClickEvents() {
-  console.log("inside the bindClickEvents")
-  $('.home-questions').fadeIn().on('click', showClickHandler)
-}
+// // Intercept the form submission event using JQuery
+// function bindClickEvents() {
+//   console.log("inside the bindClickEvents")
+//   $('.home-questions').fadeIn().on('click', showClickHandler)
+// }
 
-// Prevent the default action for that event from happening
-// Use jQuery to submit an AJAX post to the form's action
-function showClickHandler(event) {
-  console.log("inside the handleFormSubmit")
-  event.preventDefault()
-  var ajax_options = {
-    url: $(this).attr('href'),
-    headers: { Accept: "application/json" }
-  }
+// // Prevent the default action for that event from happening
+// // Use jQuery to submit an AJAX post to the form's action
+// function showClickHandler(event) {
+//   console.log("inside the handleFormSubmit")
+//   event.preventDefault()
+//   var ajax_options = {
+//     url: $(this).attr('href'),
+//     headers: { Accept: "application/json" }
+//   }
 
-// When the AJAX post is done, display the question details using JQuery
-  var $ajax = $.ajax(ajax_options)
-  $ajax.done(ajaxDoneHandler.bind(this))
-};
+// // When the AJAX post is done, display the question details using JQuery
+//   var $ajax = $.ajax(ajax_options)
+//   $ajax.done(ajaxDoneHandler.bind(this))
+// };
 
-// Function that handles when ajax is done, gets called in $ajax.done
+// // Function that handles when ajax is done, gets called in $ajax.done
 
-function ajaxDoneHandler(response) {
-  // var responseObject = JSON.parse(response)
-  console.log("inside the ajaxDoneHandler")
-  console.log(response)
-  console.log(this)
-  $(this).closest('li').append(response)
-}
+// function ajaxDoneHandler(response) {
+//   // var responseObject = JSON.parse(response)
+//   console.log("inside the ajaxDoneHandler")
+//   console.log(response)
+//   console.log(this)
+//   $(this).closest('li').append(response)
+// }
